e400f65b5a6aba1c759b6364e1b201c08b60cfb4
jk2937 <kaschakjonathan+github@gmail.com>
2023-12-27 23:05:25 -0800

    Begin creating demo 2

--- red_block_demo/script.js
+++ red_block_demo/script.js
@@ -5,7 +5,7 @@ ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 150, 75);
 
 main_exec_lock = false;
-main_loop_sleep = 100;
+main_loop_sleep = 10;
 
 rect_x = 0;
 rect_y = 0;
@@ -49,35 +49,22 @@ canvas.height = canvas_height;
   ctx.fillStyle = "#ff0000";
   ctx.fillRect(rect_x, rect_y, 25, 25);
   
- if(cursor_activated) {
-	  rect_x_vel = cursor_x - cursor_old_x / 2;
-	  rect_y_vel = cursor_y - cursor_old_y / 2;
-	 console.log(cursor_x + "." + cursor_y + "." + cursor_old_x + "." + cursor_old_y)
-	 console.log(rect_x_vel + "+" + rect_y_vel)
-	  cursor_old_x = cursor_x;
-	  cursor_old_y = cursor_y;
-	  /* rect_x = cursor_x;
-	  rect_y = cursor_y; */
-  }
   rect_x += rect_x_vel;
   rect_y += rect_y_vel;
-  /* rect_y_vel = rect_y_vel / 2;
-  rect_x_vel = rect_x_vel / 2; */
- 
-  rect_x = rect_x % canvas_width;
-  rect_y = rect_y % canvas_height;
-	
+  rect_y_vel += 1;
+  if(rect_y > canvas_height){
+    rect_y = -25;
+    rect_y_vel = 0;
+    rect_x = Math.floor(Math.random() * canvas_width-25);
+  }
+
   ctx.font = "14px Arial";
   ctx.fillText("cursor_activated", 10, 50)
   ctx.fillText(cursor_activated, 175, 50)
   ctx.fillText("cursor_x", 10, 65)
   ctx.fillText(cursor_x, 175, 65)
   ctx.fillText("cursor_y", 10, 80)
-  ctx.fillText(cursor_y, 175, 80) 
-  ctx.fillText("cursor_old_x", 10, 95)
-  ctx.fillText(cursor_old_x, 175, 95)
-  ctx.fillText("cursor_old_y", 10, 110)
-  ctx.fillText(cursor_old_y, 175, 110)
+  ctx.fillText(cursor_y, 175, 80)
 }
 
 function launch_loop() {
@@ -95,8 +82,6 @@ window.onload = setInterval(launch_loop, main_loop_sleep)
 var cursor_activated = false;
 var cursor_x = 0;
 var cursor_y = 0;
-var cursor_old_x = 0;
-var cursor_old_y = 0;
 
 canvas.addEventListener("mousedown", function (e) {
   cursor_activated = true;
