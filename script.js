98852ce6592369b3e3354a9ad206d6612916910d
jk2937 <kaschakjonathan+github@gmail.com>
2023-12-27 22:34:26 -0800

    Update inertia demo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 150, 75);
 
 main_exec_lock = false;
-main_loop_sleep = 100;
+main_loop_sleep = 15;
 
 rect_x = 0;
 rect_y = 0;
@@ -31,6 +31,8 @@ if(canvas_fullscreen) {
   canvas.style.border = "none";
 }
 
+cursor_activated_timer = 0;
+
 function main_exec_loop() {
   if(canvas_fullscreen_dynamic_res){
     canvas_width = window.screen.availWidth;
@@ -42,31 +44,45 @@ function main_exec_loop() {
 canvas.height = canvas_height;
     
   }
+  if(cursor_activated) {
+	  if(cursor_activated_timer > 2){
+	  rect_x_vel = (cursor_x - cursor_old_x) / 2;
+	  rect_y_vel = (cursor_y - cursor_old_y) / 2;
+	  /* rect_x = cursor_x;
+	  rect_y = cursor_y; */
+	}	  cursor_old_x = cursor_x;
+	  cursor_old_y = cursor_y;
+
+	 cursor_activated_timer++;
+  }
+else {
+	cursor_activated_timer = 0;
+}
+  rect_x += rect_x_vel;
+  rect_y += rect_y_vel;
+  /* rect_y_vel = rect_y_vel / 2;
+  rect_x_vel = rect_x_vel / 2; */
   
+  if(rect_x < -25){
+	  rect_x = canvas_width;
+  }
+  if(rect_y < -25){
+	  rect_y = canvas_height;
+  }
+  if(rect_x > canvas_width){
+	  rect_x = -25;
+  }
+  if(rect_y > canvas_height){
+	  rect_y = -25;
+  }
+	 
   ctx.fillStyle = "#000000";
   ctx.fillRect(0, 0, canvas_width, canvas_height);
   
   ctx.fillStyle = "#ff0000";
   ctx.fillRect(rect_x, rect_y, 25, 25);
   
- if(cursor_activated) {
-	  rect_x_vel = cursor_x - cursor_old_x / 2;
-	  rect_y_vel = cursor_y - cursor_old_y / 2;
-	 console.log(cursor_x + "." + cursor_y + "." + cursor_old_x + "." + cursor_old_y)
-	 console.log(rect_x_vel + "+" + rect_y_vel)
-	  cursor_old_x = cursor_x;
-	  cursor_old_y = cursor_y;
-	  /* rect_x = cursor_x;
-	  rect_y = cursor_y; */
-  }
-  rect_x += rect_x_vel;
-  rect_y += rect_y_vel;
-  /* rect_y_vel = rect_y_vel / 2;
-  rect_x_vel = rect_x_vel / 2; */
- 
-  rect_x = rect_x % canvas_width;
-  rect_y = rect_y % canvas_height;
-	
+
   ctx.font = "14px Arial";
   ctx.fillText("cursor_activated", 10, 50)
   ctx.fillText(cursor_activated, 175, 50)
@@ -78,6 +94,8 @@ canvas.height = canvas_height;
   ctx.fillText(cursor_old_x, 175, 95)
   ctx.fillText("cursor_old_y", 10, 110)
   ctx.fillText(cursor_old_y, 175, 110)
+
+  cursor_events = []
 }
 
 function launch_loop() {
@@ -97,6 +115,7 @@ var cursor_x = 0;
 var cursor_y = 0;
 var cursor_old_x = 0;
 var cursor_old_y = 0;
+var cursor_events = [];
 
 canvas.addEventListener("mousedown", function (e) {
   cursor_activated = true;
@@ -122,7 +141,8 @@ canvas.addEventListener("touchmove", function (e) {
 }, false);
 
 function update_mouse_pos(e) {
-  var rect = canvas.getBoundingClientRect()
-  cursor_x = e.clientX - rect.left,
-  cursor_y = e.clientY - rect.top
+  var rect = canvas.getBoundingClientRect();
+  cursor_x = e.clientX - rect.left;
+  cursor_y = e.clientY - rect.top;
+  cursor_events.push([cursor_x, cursor_y])
 } 
